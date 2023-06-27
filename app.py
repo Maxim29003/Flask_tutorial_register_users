143bd88ad3751abbd25ae63f8ed56476fc40fa35
Maxim <anufrievm077@gmail.com>
2023-06-27 17:08:04 +0300

    create register.html, def register

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, url_for, render_template
+from flask_bcrypt import bcrypt
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+import email_validator
+import sqlite3
+
+
+app = Flask(__name__)
+app.secret_key = "nsdfgndflksjgnsdfjlg"
+
+class RegForm(FlaskForm):
+    name = StringField(label='Name', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Register')
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    reg_form = RegForm()
+    if reg_form.validate_on_submit():
+        print(f"Name:{reg_form.name.data}, E-mail:{reg_form.email.data},Password:{reg_form.password.data}")
+    return render_template("register.html", form=reg_form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
