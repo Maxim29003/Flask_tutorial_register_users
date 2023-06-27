3c261777c1a5ce2a2b923e6357bc3ae4a5837b4b
Maxim <anufrievm077@gmail.com>
2023-06-27 19:53:22 +0300

    add db and create new users

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, render_template
-from flask_bcrypt import bcrypt
+from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
@@ -8,6 +8,7 @@ import sqlite3
 
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 app.secret_key = "nsdfgndflksjgnsdfjlg"
 
 class RegForm(FlaskForm):
@@ -22,13 +23,80 @@ class LogForm(FlaskForm):
     password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField(label='Register')
 
+def create_database():
+    conn = sqlite3.connect('users_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+         id INTEGER PRIMARY KEY,
+         name TEXT,
+         email TEXT,
+         password TEXT
+    );
+    ''')
+    conn.commit()
+    conn.close()
+
+def is_user_name(name):
+    conn = sqlite3.connect('users_data.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE name = ?", (name,))
+    row = cursor.fetchone()
+    conn.commit()
+    conn.close()
+    if row is None:
+        return False
+    else:
+        return True
+
+def is_user_email(email):
+    conn = sqlite3.connect('users_data.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+    row = cursor.fetchone()
+    conn.commit()
+    conn.close()
+    if row is None:
+        return False
+    else:
+        return True
+
+def add_user(name, email, password):
+    conn = sqlite3.connect('users_data.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", (name, email, password))
+    conn.commit()
+    conn.close()
+
+def show_db():
+     conn = sqlite3.connect('users_data.db')
+     cursor = conn.cursor()
+     cursor.execute("SELECT * FROM users ")
+     rows = cursor.fetchall()
+     for row in rows:
+        print(row)
+     conn.commit()
+     conn.close()
+
+
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     reg_form = RegForm()
     if reg_form.validate_on_submit():
-        print(f"Name:{reg_form.name.data}, E-mail:{reg_form.email.data},Password:{reg_form.password.data}")
-    return render_template("register.html", form=reg_form)
+        if is_user_name(reg_form.name.data):
+            return render_template("register.html", form=reg_form, message='Такое имя уже существует')
+
+        if is_user_email(reg_form.email.data):
+            return render_template("register.html", form=reg_form, message='Такой emil уже существует')
+
+        name = reg_form.name.data
+        email = reg_form.email.data
+        password = bcrypt.generate_password_hash(reg_form.password.data).decode('utf-8')
+        add_user(name, email, password)
+        show_db()
+        
+    return render_template("register.html", form=reg_form, message = '')
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -39,4 +107,5 @@ def login():
     return render_template("login.html", form=log_form)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    create_database()
+    app.run(debug=True, port=8000)
